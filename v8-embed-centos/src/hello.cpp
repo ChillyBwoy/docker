777f544107e29cbe0017d5f31a9350f824a46a5a
Eugene Cheltsov <e.cheltsov@corp.mail.ru>
2018-06-09 19:11:14 +0300

    凸ಠ益ಠ)凸

--- v8-embed-centos/src/hello.cpp
+++ v8-embed-centos/src/hello.cpp
@@ -2,104 +2,128 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iostream>
-#include <fstream>
-#include <streambuf>
+#include <map>
 #include <string>
 
 #include "libplatform/libplatform.h"
 #include "v8.h"
 
+using std::string;
+using std::map;
 
-using namespace std;
+using v8::Context;
+using v8::Isolate;
+using v8::Local;
+using v8::MaybeLocal;
+using v8::NewStringType;
+using v8::Script;
+using v8::String;
+
+void ParseOptions(int argc, char* argv[], map<string, string>* options, string* file) {
+  for (int i = 1; i < argc; i++) {
+    string arg = argv[i];
+    size_t index = arg.find('=', 0);
+    if (index == string::npos) {
+      *file = arg;
+    } else {
+      string key = arg.substr(0, index);
+      string value = arg.substr(index+1);
+      (*options)[key] = value;
+    }
+  }
+}
+
+MaybeLocal<String> ReadFile(Isolate* isolate, const string& name) {
+  FILE* file = fopen(name.c_str(), "rb");
+
+  if (file == NULL) {
+    return MaybeLocal<String>();
+  }
+
+  fseek(file, 0, SEEK_END);
+  size_t size = ftell(file);
+  rewind(file);
+
+  std::unique_ptr<char> chars(new char[size + 1]);
+  chars.get()[size] = '\0';
+
+  for (size_t i = 0; i < size;) {
+    i += fread(&chars.get()[i], 1, size - i, file);
+
+    if (ferror(file)) {
+      fclose(file);
+      return MaybeLocal<String>();
+    }
+  }
+
+  fclose(file);
+
+  MaybeLocal<String> result = String::NewFromUtf8(
+    isolate,
+    chars.get(),
+    NewStringType::kNormal,
+    static_cast<int>(size)
+  );
+
+  return result;
+}
 
 
 int main(int argc, char* argv[]) {
   // Initialize V8.
   v8::V8::InitializeICUDefaultLocation(argv[0]);
   v8::V8::InitializeExternalStartupData(argv[0]);
-  unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
   v8::V8::InitializePlatform(platform.get());
   v8::V8::Initialize();
 
   // Create a new Isolate and make it the current one.
-  v8::Isolate::CreateParams create_params;
+  Isolate::CreateParams create_params;
   create_params.array_buffer_allocator =
       v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-  v8::Isolate* isolate = v8::Isolate::New(create_params);
+  Isolate* isolate = Isolate::New(create_params);
+
   {
-    v8::Isolate::Scope isolate_scope(isolate);
+    Isolate::Scope isolate_scope(isolate);
 
     // Create a stack-allocated handle scope.
     v8::HandleScope handle_scope(isolate);
 
     // Create a new context.
-    v8::Local<v8::Context> context = v8::Context::New(isolate);
+    Local<Context> context = Context::New(isolate);
 
     // Enter the context for compiling and running the hello world script.
-    v8::Context::Scope context_scope(context);
+    Context::Scope context_scope(context);
 
     {
       // std::string js_source = read_js("hello_world.js");
       // Create a string containing the JavaScript source code.
-      v8::Local<v8::String> source =
-          v8::String::NewFromUtf8(
-            isolate,
-            "const hai = (name) => `Oh, privet, ${name}!`;\nhai('Suka');",
-            v8::NewStringType::kNormal).ToLocalChecked();
+      string file;
+      map<string, string> options;
+      ParseOptions(argc, argv, &options, &file);
+
+      if (file.empty()) {
+        fprintf(stderr, "No script was specified.\n");
+        return 1;
+      }
+
+      Local<String> source;
+      if (!ReadFile(isolate, file).ToLocal(&source)) {
+        fprintf(stderr, "Error reading file\n");
+        return 1;
+      }
 
       // Compile the source code.
-      v8::Local<v8::Script> script =
-          v8::Script::Compile(context, source).ToLocalChecked();
+      Local<Script> script = Script::Compile(context, source).ToLocalChecked();
 
       // Run the script to get the result.
-      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+      Local<v8::Value> result = script->Run(context).ToLocalChecked();
 
       // Convert the result to an UTF8 string and print it.
-      v8::String::Utf8Value utf8(isolate, result);
+      String::Utf8Value utf8(isolate, result);
       printf("%s\n", *utf8);
-    }
-
-    {
-      // Use the JavaScript API to generate a WebAssembly module.
-      //
-      // |bytes| contains the binary format for the following module:
-      //
-      //     (func (export "add") (param i32 i32) (result i32)
-      //       get_local 0
-      //       get_local 1
-      //       i32.add)
-      //
-      const char* csource = R"(
-        let bytes = new Uint8Array([
-          0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01,
-          0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07,
-          0x07, 0x01, 0x03, 0x61, 0x64, 0x64, 0x00, 0x00, 0x0a, 0x09, 0x01,
-          0x07, 0x00, 0x20, 0x00, 0x20, 0x01, 0x6a, 0x0b
-        ]);
-        let module = new WebAssembly.Module(bytes);
-        let instance = new WebAssembly.Instance(module);
-        instance.exports.add(3, 4);
-      )";
-
-      // Create a string containing the JavaScript source code.
-      v8::Local<v8::String> source =
-          v8::String::NewFromUtf8(isolate, csource, v8::NewStringType::kNormal)
-              .ToLocalChecked();
-
-      // Compile the source code.
-      v8::Local<v8::Script> script =
-          v8::Script::Compile(context, source).ToLocalChecked();
-
-      // Run the script to get the result.
-      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-
-      // Convert the result to a uint32 and print it.
-      uint32_t number = result->Uint32Value(context).ToChecked();
-      printf("3 + 4 = %u\n", number);
     }
   }
 
